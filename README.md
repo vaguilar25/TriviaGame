844e5eddf50c93cd4e5f7a2a96ca7b9f7784d462
vaguilar25 <vivian.aguilar@gmail.com>
2018-11-16 22:07:55 -0600

    Finish Readme and styling

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # TriviaGame
+Create trivia form with multiple choices .
+
+The player will have a limited amount of time to finish the quiz. 
+
+The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+
+
+The player can't pick more than one answer per question.
+
+I also show a countdown timer.
+
+## Topics that we have covered
+* Use of Git: Creating a repository
+```
+    * clone 
+    * add 
+    * commit 
+    * push
+```
+* HTML Structure: 
+```
+    H1, H2, Hr, Img, Form, Div, Span, link css
+```
+* CSS: 
+```
+    Classes, Id 
+```
+```
+    CSS properties: 
+
+    font-family, background-image,clear, font-weight, image
+
+```
+```
+    Bootstrap: 
+
+    Card, Grid, Containers
+
+```
+
+
+* Use of JQuery
+```
+    * Write to the DOM
+    * Arrays
+    * Functions
+    * Conditions
+    * Objects
+    * Switch 
+    * JavaScript clearInterval()
+    * JavaScript setInterval()
+    
+```
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/) - The editor used
+* [GitHub](https://github.com/) - Version Control
+
+## Versioning
+
+I use [GitHub](https://github.com/) for versioning. For the versions available, see the [tags on this repository](https://github.com/vaguilar25/unit-4-game). 
+
+## Authors
+
+* **Vivian Aguilar** 
+
+## Acknowledgments
+
+* Jerome Chenette
+* Sasha Pastel
+* Jimmy Tu
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,136 +1,245 @@
 $(function () {
-    //object to hold questions
-    function gameTrivia(question, op1, op2, op3, op4, answer, point, user_answer, inlineRadio) {
-        this.question = question;
-        this.op1 = op1;
-        this.op2 = op2;
-        this.op3 = op3;
-        this.op4 = op4;
-        this.answer = answer;
-        this.points = point;
-        this.user_answer = user_answer;
-        this.radio = inlineRadio;
 
-    }
-    //questions and answers
-    var trivia1 = new gameTrivia("Question 1", "op1", "op2", "op3", "op4", "op1", 5, "", "inlineRadio1");
-    createTriviaDom(trivia1);
-    var trivia2 = new gameTrivia("Question 2", "op1", "op2", "op3", "op4", "op1", 5, "", "inlineRadio2");
-    createTriviaDom(trivia2);
-    var trivia3 = new gameTrivia("Question 3", "op1", "op2", "op3", "op4", "op1", 5, "", "inlineRadio3");
-    createTriviaDom(trivia3);
+    function startGame() {
+        //object to hold questions
+        function gameTrivia(question, op1, op2, op3, op4, answer, user_answer, inlineRadio) {
+            this.question = question;
+            this.op1 = op1;
+            this.op2 = op2;
+            this.op3 = op3;
+            this.op4 = op4;
+            this.answer = answer;
 
-    var gamePoints =0;
-    // create Dom for questions
-    function createTriviaDom(trivia) {
-        //Form 
-        var formQuestions = $("<form>");
-        formQuestions.attr("id","triviaHead" + trivia.radio);
-        $(".formContainer" ).prepend(formQuestions);
+            this.user_answer = user_answer;
+            this.radio = inlineRadio;
 
-        var br = $("<br>");
-        $("#triviaHead"+ trivia.radio).append(br);
-        //Question
-        var labelQuestion = $("<label>");
-        labelQuestion.attr("answer", trivia.answer);
-        labelQuestion.attr("for", trivia.radio);
-        labelQuestion.text(trivia.question);
-        $("#triviaHead"+ trivia.radio).append(labelQuestion);
+        }
+        //questions and answers
+        var trivia1 = new gameTrivia(
+            "Which actor played Freddie Mercury in the 2018 film Bohemian Rhapsody?",
+            "Rami Malek",
+            "Tom Hanks",
+            "Will Smith",
+            "Clint Eastwood",
+            "Rami Malek",
+            "", "inlineRadio1");
 
-        var br = $("<br>");
-        $("#triviaHead"+ trivia.radio).append(br);
+        createTriviaDom(trivia1);
 
-        //Div to hold options
-        var divContainer = $("<div>");
-        divContainer.addClass("form-check form-check-inline");
-        divContainer.attr("id", "answersOp" + trivia.radio);
-        $("#triviaHead"+ trivia.radio).append(divContainer);
-        //build answer object op name
-        var opName = "op";
+        var trivia2 = new gameTrivia(
+            "In which Disney film do two cats sing “The Siamese Cat Song”?",
+            "The Aristocats",
+            "The Cat in the Hat",
+            "Lady and the Tramp",
+            "Oliver and Company",
+            "Lady and the Tramp",
+            "", "inlineRadio2");
+
+        createTriviaDom(trivia2);
+
+        var trivia3 = new gameTrivia(
+            "What popular Disney movie is set near Salem, Massachusetts in the years 1693 and 1993?",
+            "The rescuers",
+            "Candle Shoe",
+            "Hocus Pocus",
+            "Robin Hood",
+            "Hocus Pocus",
+            "", "inlineRadio3");
+
+        createTriviaDom(trivia3);
+
+        var trivia4 = new gameTrivia(
+            "What is Shawshank, in the movie The Shawshank Redemption?",
+            "The prison",
+            "A fabric",
+            "City",
+            "House",
+            "The prison",
+            "", "inlineRadio4");
+
+        createTriviaDom(trivia4);
+
+        var trivia5 = new gameTrivia(
+            "Who plays Jack Ryan in the 2002 American spy thriller “The Sum of all Fears?",
+            "George Clooney",
+            "Ben Affleck",
+            "Brad Pitt",
+            "Will Smith",
+            "Ben Affleck",
+            "", "inlineRadio5");
+
+        createTriviaDom(trivia5);
 
 
-        //iterate 4 times because there are 4 answers
-        for (i = 1; i <= 4; i++) {
-            //concatenate i and the build the variable name
-            var option = opName.concat(i);
-            var answerVarName = "trivia." + option;
+        var trivia6 = new gameTrivia(
+            "Which actor played the fictional character Dr. Emmett Brown in the Back to the Future trilogy?",
+            "Matt Damon",
+            "Adam Sandler",
+            "Leonardo DiCaprio",
+            "Christopher Lloyd",
+            "Christopher Lloyd",
+            "", "inlineRadio1");
+
+        createTriviaDom(trivia6);
 
 
-            //Create options
-            var liAnswer = $("<input>");
-            liAnswer.addClass("form-check-input");
-            liAnswer.attr("id", "inlineRadio" + i);
-            liAnswer.attr("type", "radio");
-            liAnswer.attr("name", "inlineRadioOptions");
-            liAnswer.attr("answer", eval(answerVarName));
-            liAnswer.attr("correctanswer", trivia.answer);
-            liAnswer.attr("points", trivia.points);
-            liAnswer.attr("question", trivia.question);
-            // liAnswer.text(eval(answerVarName));
-            $("#answersOp" + trivia.radio).append(liAnswer);
+        var totalQuestions = 6;
+        // create Dom for questions
+        function createTriviaDom(trivia) {
+            //Form 
+            var formQuestions = $("<form>");
+            formQuestions.attr("id", "triviaHead" + trivia.radio);
+            $(".formContainer").prepend(formQuestions);
 
-            //create labels for options
-            var label = $("<label>");
-            label.addClass("form-check-label");
-            label.attr("for", "inlineRadio" + i);
-            label.text(eval(answerVarName));
-            $("#answersOp" + trivia.radio).append(label);
+            var br = $("<br>");
+            $("#triviaHead" + trivia.radio).append(br);
+            //Question
+            var labelQuestion = $("<label>");
+            labelQuestion.addClass("font-weight-bold");
+            labelQuestion.attr("answer", trivia.answer);
+            labelQuestion.attr("for", trivia.radio);
+            labelQuestion.text(trivia.question);
+            $("#triviaHead" + trivia.radio).append(labelQuestion);
 
+
+
+            var br = $("<br>");
+            $("#triviaHead" + trivia.radio).append(br);
+
+            //Div to hold options
+            var divContainer = $("<div>");
+            divContainer.addClass("form-check form-check-inline");
+            divContainer.attr("id", "answersOp" + trivia.radio);
+            $("#triviaHead" + trivia.radio).append(divContainer);
+           
+            //build answer object op name
+            var opName = "op";
+
+
+            //iterate 4 times because there are 4 answers
+            for (i = 1; i <= 4; i++) {
+               
+                //concatenate i and the build the variable name
+                var option = opName.concat(i);
+                var answerVarName = "trivia." + option;
+
+
+                //Create options
+                var liAnswer = $("<input>");
+                liAnswer.addClass("form-check-input");
+                liAnswer.attr("id", "inlineRadio" + i);
+                liAnswer.attr("type", "radio");
+                liAnswer.attr("name", "inlineRadioOptions");
+                liAnswer.attr("answer", eval(answerVarName));
+                liAnswer.attr("questionContainer", "#triviaHead" + trivia.radio);
+                liAnswer.attr("correctanswer", trivia.answer);
+
+                liAnswer.attr("question", trivia.question);
+                // liAnswer.text(eval(answerVarName));
+                $("#answersOp" + trivia.radio).append(liAnswer);
+
+                //create labels for options
+                var label = $("<label>");
+                label.addClass("form-check-label");
+                label.attr("for", "inlineRadio" + i);
+                label.text(eval(answerVarName));
+                $("#answersOp" + trivia.radio).append(label);
+
+
+
+
+            }
 
 
 
         }
 
-        
+        //Get the radio button clicked with the data attributes
+        var correctAnswerCount = 0;
+        var incorrectAnswerCount = 0;
+        var notanswered = 0;
+        jQuery(document).delegate("input[type='radio']", "click",
+            function (e) {
 
+                var answerClicked = $(this).attr("answer");
+                var correctAnswer = $(this).attr("correctanswer");
+
+                var questionSelected = $(this).attr("questionContainer")
+                $(questionSelected).click(false);
+
+                if (answerClicked === correctAnswer) {
+
+                    correctAnswerCount++
+                   
+                } else {
+                    incorrectAnswerCount++
+                    
+                }
+
+
+
+            });
+
+        //On click done delete trivia and show score
+        $("#buttonDone").on("click", function () {
+            stop();
+
+        });
+
+        function showCorrectAnswer() {
+            unansweredQuestions = (totalQuestions - correctAnswerCount - incorrectAnswerCount)
+            $("#show-number").html("<br><br><br><br><h2 class=text-center>Your Results!</h2><h3 class=text-center >Correct Answers: " + correctAnswerCount + "</h3>"
+                + "<h3 class=text-center>Incorrect Answers: " + incorrectAnswerCount + "</h3>"
+                + "<h3 class=text-center>Unsanswered Questions: " + unansweredQuestions + "</h3>");
+
+        }
+        //clear trivia questions
+        function clearTrivia() {
+            $(".formContainer").remove();
+        }
+
+        var intervalId;
+        var number = 40;
+        function run() {
+            //clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+           
+        }
+        //  The decrement function.
+        function decrement() {
+            //  Decrease number by one.
+            number--;
+            //  Show the number in the #show-number tag.
+            $("#show-number").html("<h3>Time Remaining: " + number + "</h3>");
+            //  Once number hits zero...
+            if (number === 0) {
+                //  ...run the stop function.
+                stop();
+                // show the next queestion
+            }
+        }
+
+        //  The stop function
+        function stop() {
+
+            //  Clears our intervalId
+            //  We just pass the name of the interval
+            //  to the clearInterval function.
+            clearInterval(intervalId);
+            clearTrivia();
+            showCorrectAnswer();
+        }
+
+        //  Execute the run function.
+
+        run();
     }
+    ///Start Trivia
+    $("#buttonStart").on("click", function () {
 
+        $(".startGame").remove();
+        $("#buttonDone").show();
+        startGame();
 
+    });
 })
-
-//Get the radio button clicked with the data attributes
-var correctAnswerCount=0;
-var incorrectAnswerCount=0;
-var notanswered=0;
-jQuery(document).delegate( "input[type='radio']", "click",
-    function(e){
-    var questionClicked = $(this).attr("question");
-    var answerClicked=$(this).attr("answer");
-    var correctAnswer=$(this).attr("correctanswer");
-    
-
-   // console.log("entra" + test);
-    if (answerClicked === correctAnswer) {
-       
-        correctAnswerCount++
-        console.log("correct: " + correctAnswerCount);
-    } else {
-        incorrectAnswerCount++
-        console.log("incorrect" + incorrectAnswerCount);
-    }
-    
-    
-    
-});
-
-//On click done delete trivia and show score
-$("#buttonDone").on("click",function(){
-clearTrivia();
-$("#show-number").html("<h3>Correct Answers: " + correctAnswerCount + "</h3>"
-                        + "<h3>Incorrect Answers: " + incorrectAnswerCount + "</h3>");
-
-                      
-
-});
-
-//clear trivia questions
-function clearTrivia() {
-    $(".formContainer").remove();
-    
-    console.log("remove");
-}
-
-//$("input[type=radio]").click(function(event) {
-  //  var text = event.target.id;
-   // console.log("text" + text);
-//});
